216a61840aa10ef10a82f85b6f76fed05ef3b68b
Taejung Kim <immcoc1@gmail.com>
2023-08-27 00:45:20 +0900

    feature: basic pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import styles from "./page.module.css"
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <main className={styles.main}>
+      <img src="/logo/logotype_web_long.svg" className={styles.logo}/>
+      <text className="W1">
+        Hello, <br /> Next.js!
+      </text>
+    </main>
+  );
 }
---
+++ app/store/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <text className="W2">스토어 페이지</text>
+}
