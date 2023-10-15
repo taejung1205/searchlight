bd8d8ab64c8fa972add3181ef1919212ecd1aeb5
Taejung Kim <immcoc1@gmail.com>
2023-10-15 15:14:54 +0900

    feature: splash animation effect

--- app/fair/2023/page.tsx
+++ app/fair/2023/page.tsx
@@ -1,13 +1,34 @@
 "use client";
 
 import { Header } from "@/components/header/header";
+import style from "./page.module.css";
+import { useState } from "react";
 
 export default function Page() {
-    return (
-      <div>
-        <Header currentPage={"/fair/2023"} />
-        <p className="W2">페어 페이지</p>
-      </div>
-    );
-  }
-  
+  const [isSplash, setIsSplash] = useState<boolean>(true);
+  return (
+    <div>
+      <Header currentPage={"/fair/2023"} />
+      {isSplash ? <SplashImages onClick={() => setIsSplash(false)}/> : <div></div>}
+    </div>
+  );
+}
+
+function SplashImages({ onClick }: { onClick: () => void }) {
+  return (
+    <div onClick={onClick} className={style.splash_image_box}>
+      <img
+        src="/artwork/윤지훈/윤지훈_Happy Dog Lamp.jpg"
+        className={style.splash_image_1}
+      />
+      <img
+        src="/artwork/윤지훈/윤지훈_Homebody Lamp.png"
+        className={style.splash_image_2}
+      />
+      <img
+        src="/artwork/윤지훈/윤지훈_Mulberry.jpg"
+        className={style.splash_image_3}
+      />
+    </div>
+  );
+}
