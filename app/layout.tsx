afa65dc4dfb025c7f2327ea46c3939f3e2a5d3ee
Taejung Kim <immcoc1@gmail.com>
2023-11-11 16:44:47 +0900

    fix: website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "써치라이트 SEARCHLIGHT",
+  title: "서치라이트 SEARCHLIGHT",
   description: "By LOFA SEOUL",
 };
 
